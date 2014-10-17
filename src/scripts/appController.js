272a226d4ee608e8050cd20538e289a176a394a0
anvaka <anvaka@gmail.com>
2014-10-16 19:52:17 -0700

    initial commit

---
+++ src/scripts/appController.js
@@ -0,0 +1,5 @@
+require('an').controller(AppController);
+
+function AppController($scope) {
+  $scope.greeting = 'The mind is everything. What you think you become.';
+}
---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+require('./appController');
+
+var ngApp = angular.module('allnpmviz3d', []);
+
+require('an').flush(ngApp);
+
+angular.bootstrap(document, [ngApp.name]);
+
+module.exports = ngApp;
