e1d6ea898c334f0afbd0a584ac90086a9676f5cb
anvaka <anvaka@gmail.com>
2014-10-17 22:42:37 -0700

    Acutally don't need two variables

--- src/scripts/view/graphView.js
+++ src/scripts/view/graphView.js
@@ -77,8 +77,6 @@ function init3dView() {
   }
 
   function onWindowResize() {
-    windowHalfX = window.innerWidth / 2;
-    windowHalfY = window.innerHeight / 2;
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
