cffdeb1f65ff5d7951b2e4c4a19334384e2d93b4
anvaka <anvaka@gmail.com>
2014-11-01 19:33:31 -0700

    Properly handling malformed regexes

--- src/scripts/search/matcher.js
+++ src/scripts/search/matcher.js
@@ -2,12 +2,19 @@
  * This module checks whether node matches given pattern
  */
 module.exports = function(pattern) {
-  var rNameMatch = new RegExp(pattern, 'ig');
-
+  var rNameMatch = compileRegex(pattern);
   return {
     isMatch: function(data) {
       // a simple name based match. Could be extended to complex filters
-      return data && data.label && data.label.match(rNameMatch);
+      return rNameMatch && data && data.label && data.label.match(rNameMatch);
     }
   };
 };
+
+function compileRegex(pattern) {
+  try {
+    return new RegExp(pattern, 'ig');
+  } catch (e) {
+    // this cannot be compiled. Ignore it.
+  }
+}
