89dc2da7ad655f21ccc4464aec1b7fe312bba0e4
Eric Zhao <ericlzhao28@gmail.com>
2018-05-19 05:04:19 -0700

    Dockerized application, removed the crap bash deployment script.

--- deploy-gh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm -rf dist || exit 0;
-gulp build
-( cd dist
- git init
- git add .
- git commit -m "Deployed to Github Pages"
- git push --force git@github.com:anvaka/allnpmviz3d.git master:gh-pages
-)
